a8d0d4d0116450a21aeef6ca1115095f3c62216b
Darren Tsung <darren@onesignal.com>
2018-08-01 10:36:50 -0700

    Revert namespace round-robin

--- src/dns/c_ares.rs
+++ src/dns/c_ares.rs
@@ -1,13 +1,7 @@
-use c_ares_resolver::{Options, FutureResolver};
+use c_ares_resolver::FutureResolver;
 
 lazy_static! {
     pub static ref GLOBAL_RESOLVER: FutureResolver = {
-        let mut options = Options::default();
-
-        // enable round-robin rotating of the nameservers
-        // returned, this way we can balance out load
-        options.set_rotate();
-
-        FutureResolver::with_options(options).unwrap()
+        FutureResolver::new().unwrap()
     };
 }
