b139ae3270fb710fc6c7d506078e7a7a166c889b
Darren Tsung <darren@onesignal.com>
2018-08-01 09:26:04 -0700

    Enable round-robin rotating of the DNS resolver

--- src/dns/c_ares.rs
+++ src/dns/c_ares.rs
@@ -1,7 +1,13 @@
-use c_ares_resolver::FutureResolver;
+use c_ares_resolver::{Options, FutureResolver};
 
 lazy_static! {
     pub static ref GLOBAL_RESOLVER: FutureResolver = {
-        FutureResolver::new().unwrap()
+        let mut options = Options::default();
+
+        // enable round-robin rotating of the nameservers
+        // returned, this way we can balance out load
+        options.set_rotate();
+
+        FutureResolver::with_options(options).unwrap()
     };
 }
